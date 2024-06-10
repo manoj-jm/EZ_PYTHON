4cfc837acb8693f9271ea36b6785c074c50242d3
manoj <manojjmvr49@gmail.com>
2024-06-10 15:59:59 +0530

    jailer problem

--- jailer.py
+++ jailer.py
@@ -1,7 +1,14 @@
-# prisoner = [1,2,3,4,5]
-# co = 10
-# r = 3
-# no_prisoner = int(input("enter the no_prisoner :"))
-# no_chacolate = int(input("enter the no_chacolate :"))
-# random_prisoner = int(input("enter the random_prisoner :"))
+n = int(input("enter the no_prisoner :"))
+c = int(input("enter the no_chacolate :"))
+s = int(input("enter the start_prisoner :"))
+ 
+# c = 10 , n=6  , s = 3 
+# output = 6
+# c_distribution =       3 4 5 6 7 8 9 10 11 12 
+# no_prisoner_circular = 3 4 5 6 1 2 3  4  5  6 
 
+last_prisoner = ((s+c) -1) % n
+if last_prisoner == 0:
+  print(n)
+else:
+  print(last_prisoner)
--- pattern1.py
+++ pattern1.py
@@ -6,3 +6,4 @@ for i in range(n):
     else:
       print(' ' , end=' ')
   print()
+
