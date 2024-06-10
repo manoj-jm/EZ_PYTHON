8f93acbe0b27e8ef61bfa3140555b81759e3c44d
manoj <manojjmvr49@gmail.com>
2024-06-10 16:17:41 +0530

    add to folder day1

---
+++ day1/login.py
@@ -0,0 +1,6 @@
+#write a program to build login system using funcitons .  the function name should be login and register 
+# 1 . ask user to enter the details for registration  and   out of all its details only user name and passwrod should be stored
+# 2  . now this funciton should ask user to enter user name and password and check whether if these match with the register details , lgoin successfull otherwise repeat this login process , saying invalid credentials.
+
+def registeration():
+  welcome
