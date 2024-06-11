1a3332c410a0f7e50fa8448cb5b889cb58360379
manoj <manojjmvr49@gmail.com>
2024-06-11 18:37:16 +0530

    quesiton file is deleted content

--- day2/question.py
+++ day2/question.py
@@ -1,19 +1,2 @@
 #find next greather value for every element 
 #[14,2,16,4,3,5]=>[16,16,-1,5,-1,-1] using stacks
-
-
-class pra:
-  def __init__(self,var) -> None:
-    print(var)
-    self.var = var
-    print(self.var)
-    self.vari = 20
-    self.get(self.var)
-
-  def get(self,man):
-    print(self.vari)
-    man = 109
-    print(man)
-
-
-obj = pra(289)
