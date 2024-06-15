3fc17f41b66bdb98fcad1a516069c8854151caf5
manoj <manojjmvr49@gmail.com>
2024-06-15 09:31:08 +0530

    patterns

---
+++ pattern.py
@@ -0,0 +1,30 @@
+for i in range(0,4):
+  print('*'*4)
+
+#pattern2
+for i in range(0,4+1):
+  print("*"*i)
+
+#pattern3
+for i in range(1,4+1):
+  for j in range(1,i+1):
+    print(j,end=" ")
+  print("")
+
+#pattern4
+for i in range(1,4+1):
+  for j in range(1,i+1):
+    print(i,end=' ')
+  print('')
+
+#pattern5
+for i in range(5,0,-1):
+  print("*"*i)
+
+#pattern6
+for i in range(5,0,-1):
+  for j in range(1,i+1):
+    print(j,end='')
+  print('')
+
+
