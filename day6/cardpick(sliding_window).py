8b78fba9195d909700c76ed9bcfbe73340d08e21
manoj <manojjmvr49@gmail.com>
2024-06-15 16:32:22 +0530

    cardPick

---
+++ day6/cardpick(sliding_window).py
@@ -0,0 +1,12 @@
+#using  sliding window technique
+cards = [3,4,2,3,3,5,2,4,7]
+min_value = {}
+min_distance = float('inf')
+
+for i, card in enumerate(cards):
+    if card in min_value:
+        window_size = i - min_value[card] + 1
+        min_distance = min(min_distance, window_size)
+    min_value[card] = i
+  
+print("minimum cards to pick for pair : ", min_distance)
