7bb0d768a2e43eef3fb0163cfc29c6123c5ee930
manoj <manojjmvr49@gmail.com>
2024-06-16 14:46:26 +0530

    pattern

--- pattern.py
+++ pattern.py
@@ -28,3 +28,13 @@ for i in range(5,0,-1):
   print('')
 
 
+# #pattern7
+# for i in range(1,6):
+#   for j in range(i*5-1):
+#     print("*" * j, end='')
+#   for j in range(i):
+#     print("*"*i,end='')
+#   print('')
+
+
+    
