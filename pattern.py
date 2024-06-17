b679fe37c4e5b278664fce6c42d0e6bc28e87b24
manoj <manojjmvr49@gmail.com>
2024-06-17 09:52:12 +0530

    pattern updated

--- pattern.py
+++ pattern.py
@@ -29,12 +29,99 @@ for i in range(5,0,-1):
 
 
 # #pattern7
-# for i in range(1,6):
-#   for j in range(i*5-1):
-#     print("*" * j, end='')
-#   for j in range(i):
-#     print("*"*i,end='')
-#   print('')
+n=5
+for i in range(n+1):
+  print(" " * (n-i) + "*" * (2*i -1))
+
+#patter8
+n=5
+print('pattern 8 ')
+for i in range(n,0,-1):
+  print(" " * (n-i) + "*" * (2*i -1))
 
 
-    
+# daimond 9
+for i in range(n):
+  print(" " * (n-i) + "*" * (2*i -1))
+for i in range(n,0,-1):
+  print(" " * (n-i) + "*" * (2*i -1))
+  
+
+#pattern 10
+for i in range(n):
+  print("*"*i)
+for i in range(n,0,-1):
+  print("*"*i)
+
+
+#pattern11
+m = 1
+for i in range(1,6):
+  for j in range(1,i+1):  
+    if (i+j)%2 == 0:
+      print(1 , end='')
+      # m=0
+    else:
+      print(0 , end='')
+      # m=1
+  print('')
+
+  
+
+#pattern12
+n=4
+space = 2 * (n -1)
+
+print(space)
+for i in range(1,n+1):
+  for j in range(1,i+1):
+    print(j,end='')
+
+  for k in range(space,0,-6):
+    print(" " * k ,end='')
+
+  for j in range(i,0,-1):
+    print(j,end='')
+  print('')
+  space-=2
+
+#pattern 13
+start = 1
+n = 5
+for i in range(n+1):
+  for j in range(i):
+    print(start,end=' ')
+    start+=1
+  print('')
+
+
+#pattern 14
+n = 5
+for i in range(1,n+1):
+  char = 65
+  for j in range(char,char+i):
+    print(chr(char), end=' ')  
+    char+=1
+  print()
+
+#pattern 15
+n = 5 
+print("\n")
+for i in range(n,0,-1):
+  ch = 65
+  for j in range(i):
+    print(chr(ch),end=' ')
+    ch+=1
+  print('')
+
+
+#pattern 16
+n = 5
+c = 65 
+for i in range(1,n+1):
+  for j in range(i):
+    print(chr(c),end=' ')
+  c+=1
+  print('')
+
+# pattern 17
