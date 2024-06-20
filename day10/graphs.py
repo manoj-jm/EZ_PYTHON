409fdf385c08949353c6b5be1d57a3514f2dc227
manoj <jmanojjmvr49@gmail.com>
2024-06-20 11:51:21 +0530

    graphs

--- day10/graphs.py
+++ day10/graphs.py
@@ -14,12 +14,6 @@ def dfs(graph,src,visited_arr,stack_dfs):
 
   print(stack_dfs.pop())
 
-    
-
-
-  
-
-
 #dict to store the graph in the form of tuple (start,end,weight )
 graph = {
   1:[(1,2,0),(1,3,0)],
@@ -33,7 +27,6 @@ graph = {
 }
 
 dict_visit = {}
-# keys = {1:False,2:False,3:False,4:False,5:False,6:False,7:False,8:False}
 visited_arr = dict_visit.fromkeys(graph,False)
 print(visited_arr)
 stack_dfs = []
