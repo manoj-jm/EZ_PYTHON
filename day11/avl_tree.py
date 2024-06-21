b73bf9c9d0c64c3b659c553f1daa445e21c9d1db
manoj <jmanojjmvr49@gmail.com>
2024-06-21 12:01:34 +0530

    avl_tree modified

--- day11/avl_tree.py
+++ day11/avl_tree.py
@@ -8,25 +8,61 @@ class node:
     self.right = None
     self.height = 1
 
+def getHeight(root):
+  if not root:
+    return 0
+  return root.height
+
+
+def getBF(root):
+  if not root:
+    return 0
+  return getHeight(root.left) - getHeight(root.right) 
+
+def leftRotation(A):
+  B=A.right
+  temp = B.left
+
+  B.left = A
+  A.right = temp
+
+  A.height = 1 + max(getHeight(A.left),getHeight(A.right))
+  B.height = 1 + max(getHeight(B.left),getHeight(B.right))
+
+  
+  return B
+
+def rightRotation(A):
+  B=A.left
+  temp = B.right
+
+  B.right = A
+  A.left = temp
+
+  A.height = 1 + max(getHeight(A.left),getHeight(A.right))
+  B.height = 1 + max(getHeight(B.left),getHeight(B.right))
+
+  return B
+
 
 def insert(root,super):
   # step 1 : insert node
-  # step 2 : balance factor
+  # step 2 : height  and balance factor
   # step 3 : rotation
   # step 4 : return root
   if not root:
     return node(super)
-  if super < root.value:
+  if super < root.val:
     root.left = insert(root.left,super)
   else:
-    root.right = insert(root.righ,super)
+    root.right = insert(root.right,super)
 
 
   root.height = 1 + max(getHeight(root.left),getHeight(root.right))
   # balance factor
   BF = getBF(root)
 
-
+  #Rotations
   #RR
   if  BF > 1 and super < root.left.val:
     return rightRotation(root)
@@ -55,6 +91,7 @@ def inorder(root):
 
 if __name__ == "__main__":
   VL = [19,98,75,7,21,34,38,27,134,100,20,0,12,17,144]
+  root = None
   for i in VL:
     root = insert(root,i)
 
