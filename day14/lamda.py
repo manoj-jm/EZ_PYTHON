37fd55d6a74ce0085a5f8e065171adf6056bf3c0
manoj <jmanojjmvr49@gmail.com>
2024-06-25 09:56:40 +0530

    lamda function

---
+++ day14/lamda.py
@@ -0,0 +1,8 @@
+# for to use the multiple function multiple times , we can use lamda function
+# it is a anonamous funciton , bec it doesn't have name of the function 
+a = lambda x : x*x
+b = lambda y : y-4
+print(a(20) , b(20))
+
+c = lambda x,y,z : x+y*z
+print(c(1,2,3))
