42085cb724d05997ad16376591540e9c9f077832
manoj <jmanojjmvr49@gmail.com>
2024-06-25 12:32:05 +0530

    knapsack(dp)

---
+++ day14/knapsack(dp).py
@@ -0,0 +1,18 @@
+# job_profit = { 'j1':20,'j2':40,'j3':10,'j4':30,'j5':50}
+# job_deadline = {'j1':2,'j2':1,'j3':2,'j4':3,'j5':1}
+
+
+def calc_max(p,w,c,n):
+  if n==0 or c == 0:
+    return 0
+  if (w[n-1]>c):
+    return calc_max(p,w,c,n-1)
+  else:
+    return max(p[n-1]+calc_max(p,w,c-w[n-1],n-1),calc_max(p,w,c,n-1))
+
+p = [5,10,15,7,8,9,4]
+w = [1,3,5,4,1,3,2]
+c = 15
+n = len(p)
+val = calc_max(p,w,c,n)
+print(val)
