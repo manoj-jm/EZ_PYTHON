ce35e71748f06aeaa7c1ce52232e1bee0a8c1fb1
manoj <jmanojjmvr49@gmail.com>
2024-06-25 10:13:43 +0530

    knapsack

--- day14/knapsack.py
+++ day14/knapsack.py
@@ -1,9 +1,11 @@
 # greedy algorithm
 # knapsack problem
-
+import math as m
 #profit and weights
 p = [5,10,15,7,8,9,4]
 w = [1,3,5,4,1,3,2]
+
+weight = int(input("enter the max weight of knapsack : "))
 
 p_w = {}
 #p/w 
@@ -16,13 +18,23 @@ for i in range(len(p)):
 L = list(p_w.items())
 print(L)
 n = len(L)
+# approach 1  using lamda function
+# L.sort(key=lambda x:x[1],reverse=True)
+# print(L)
 
-for i in range(n):
-  max = i
-  for j in range(i+1,n):
-    if L[j][1]>L[max][1]:
-      max = j
-  L[i],L[max]=L[max],L[i]
+# for i in range(n):
+#   max = i
+#   for j in range(i+1,n):
+#     if L[j][1]>L[max][1]:
+#       max = j
+#   L[i],L[max]=L[max],L[i]
+# print(L)
 
-print(L)
 
+#current weight 
+curr_w = 0
+for i in range(len(L)):
+  if curr_w < weight : 
+    curr_w += L[i][1]
+  
+print(m.ceil(curr_w))
