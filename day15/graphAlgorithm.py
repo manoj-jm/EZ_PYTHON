67c5aa43d8bab6bb3b26351e2696ae9f70d135a8
manoj <jmanojjmvr49@gmail.com>
2024-06-26 14:40:26 +0530

    graph(kruskle_problem)

--- day15/graphAlgorithm.py
+++ day15/graphAlgorithm.py
@@ -13,59 +13,32 @@ eg : if vertices are 9 and edges are 13 :
 '''
    
 #adjacency matrix 
-graph = [
-    [0, 7, -1, -1, -1, -1, 2, -1, -1],
-    [7, 0, 4, 1, -1, 5, -1, -1, -1],
-    [-1, 4, 0, -1, -1, -1, 8, -1, -1],
-    [-1, 1, -1, 0, 6, 8, 3, 3, -1],
-    [-1, -1, -1, 6, 0, -1, 6, 8, -1],
-    [-1, 5, -1, 8, -1, 0, -1, -1, -1],
-    [2, -1, 8, 3, 6, -1, 0, -1, -1],
-    [-1, -1, -1, 3, 8, -1, 9, 0, -1],
-    [-1, -1, -1, -1, -1, 2, -1, -1, 0]
-]
-
-visited = [False] * len(graph)
-min_edge = float('inf')
-x = y = -1
-
-# Finding the initial minimum edge
-for i in range(len(graph)):
-    for j in range(len(graph[i])):
-        if graph[i][j] == 0 or graph[i][j] == -1:
-            continue
-        elif min_edge > graph[i][j]:
-            min_edge = graph[i][j]
-            x = i
-            y = j
-
-print(x + 1, y + 1, min_edge)  # Initial minimum edge output
-visited[x] = True
-visited[y] = True
-mst = []
-mst.append((x + 1, y + 1, min_edge))
-
+adja_Matrix=[[0,11,7,1,-1,-1],
+             [11,0,3,-1,4,-1],
+             [7,3,0,-1,4,-1],
+             [1,-1,5,0,-1,2],
+             [-1,4,-1,-1,0,-1],
+             [-1,-1,-1,2,-1,0]]
+visited=[False]*len(adja_Matrix)
+min=float("inf")
+for i in range(len(adja_Matrix)):
+    for j in range(len(adja_Matrix[i])):
+        if adja_Matrix[i][j]<min and adja_Matrix[i][j]>0 :
+            min=adja_Matrix[i][j]
+            pos=i
+            pos1=j
+    visited[pos]=visited[pos1]=True
+spanning_tree=[(pos+1,pos1+1,min)]
 while False in visited:
-    min_edge = float('inf')  # Reset the minimum edge for the next iteration
-    next_x = next_y = -1  # Reset next_x and next_y for the next minimum edge
-
+    min=float('inf')
     for i in range(len(visited)):
-        if visited[i]:
-            for j in range(len(graph[i])):
-                if graph[i][j] == 0 or graph[i][j] == -1 or visited[j]:
-                    continue
-                elif min_edge > graph[i][j]:
-                    min_edge = graph[i][j]
-                    next_x = i
-                    next_y = j
-
-    if next_y == -1:  # If no valid edge is found, break the loop
-        break
-
-    visited[next_y] = True
-    mst.append((next_x + 1, next_y + 1, min_edge))
-
-for i in mst:
-    print(i,end="\n")
-
+        if visited[i]==True:
+            for j in range(len(adja_Matrix[i])):
+                if adja_Matrix[i][j]>0 and adja_Matrix[i][j]<min and visited[j]==False:
+                    min=adja_Matrix[i][j]
+                    pos=i
+                    pos1=j
+    visited[pos1]=True
+    spanning_tree.append((pos+1,pos1+1,min))
+print(spanning_tree)
 
