2fd5a24d069d410969130a9c705d6eac0fb0a136
manoj <jmanojjmvr49@gmail.com>
2024-06-26 10:40:33 +0530

    prims algorithm

--- day15/graphAlgorithm.py
+++ day15/graphAlgorithm.py
@@ -1,1 +1,14 @@
 # graphs algorithm 
+# edges : 1 .weighted , 2.directed 
+'''
+spanning tree : it is a tree , which mentain a property to no of vertices should  be n-1
+no of spanning trees : (n of E)+ ... + (n-1) 
+eg : if vertices are 9 and edges are 13 : 
+      13 + 12 + 11 + 10 + 9 + 8 = = 63
+
+* prim's algorithm : 
+      select the minimum edge vetrices
+      (maintian list [f,f,f,f,f,f,f,f,f])
+      slect the minimum edge at every veritces .
+'''
+
