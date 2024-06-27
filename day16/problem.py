2b33da8af67135465b8601bda8d2e75c0e83b6ff
manoj <jmanojjmvr49@gmail.com>
2024-06-27 16:02:44 +0530

    problem

---
+++ day16/problem.py
@@ -0,0 +1,14 @@
+# s = ABABABCANFKABABCNKABABCACNDA
+# P = ABABCA
+#RETURN THE INDEX OF P IN S
+s = "ABABABCANFKABABCNKABABCACNDA"
+p = "ABABCA"
+val = []
+for i in range(len(s)):
+  val.append(s[i:len(p)+i])
+print(val)
+
+if p in val:
+  print(val.index(p))
+
+    
