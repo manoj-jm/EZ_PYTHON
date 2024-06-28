0c7bd10a29f3ffa86fb12d7a6aef4ea1c420cd88
manoj <jmanojjmvr49@gmail.com>
2024-06-28 09:47:46 +0530

    lps array

--- day16/problem.py
+++ day16/problem.py
@@ -4,14 +4,17 @@
 s = "ABABABCANFKABABCNKABABCACNDA"
 p = "ABABCA"
 val = []
+# for i in range(len(s)):
+#   val.append(s[i:len(p)+i])
+# print(val)
+
+# if p in val:
+#   print(val.index(p))
+
 for i in range(len(s)):
-  val.append(s[i:len(p)+i])
-print(val)
-
-if p in val:
-  print(val.index(p))
-
-    
+  if s[i:len(p)+i]==p:
+    print(i)
+    break
 
 # LPS ARRAY
 # KMP ALGORITHM
